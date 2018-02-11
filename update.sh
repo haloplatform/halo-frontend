b11e9b4d3c8d344c434cd781a6855ff495fc8ad9
EC2 Default User <ec2-user@ip-10-50-4-93.eu-west-1.compute.internal>
2018-02-11 22:07:43 +0000

    change update file

--- update.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-git pull origin master
-aws s3 sync . s3://www.haloplatform.tech/ 
-aws s3 rm s3://www.haloplatform.tech/.git/*
-aws cloudfront create-invalidation --distribution-id E3HCA213UB494M \
-  --paths /*
